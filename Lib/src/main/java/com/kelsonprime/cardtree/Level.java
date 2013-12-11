71dbbd9219787f071707587f0491acf8e7fb438d
Kurt Nelson <kurtisnelson@gmail.com>
2013-12-11 16:35:22 -0500

    Skeleton

---
+++ Lib/src/main/java/com/kelsonprime/cardtree/Level.java
@@ -0,0 +1,8 @@
+package com.kelsonprime.cardtree;
+
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class Level extends ArrayList<View> {
+}
---
+++ Lib/src/main/java/com/kelsonprime/cardtree/Tree.java
@@ -0,0 +1,4 @@
+package com.kelsonprime.cardtree;
+
+public class Tree {
+}
